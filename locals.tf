c53d6226bb0f5c3a882106c53efacb67a385b992
kondalaraon <kondalaraon@gmail.com>
2026-04-10 21:12:41 +0530

    action

--- locals.tf
+++ locals.tf
@@ -10,6 +10,14 @@ locals {
                 {
                    Name = "${var.project}-${var.environment}"
                 },
-                var.vpc.tags
+                var.vpc_tags
             )
+
+  igw_final_tags = merge (
+                 local.common_tags,
+                {
+                   Name = "${var.project}-${var.environment}"
+                },
+                var.igw_tags          
+                  )
 }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,12 @@ variable "vpc_cidr"{
     type = string
     default = "10.0.0.0/16"
 }
+variable "vpc_tags" {
+    type = map
+     default = {}  
+}
+
+variable "igw_tags" {
+    type = map
+     default = {}  
+}
